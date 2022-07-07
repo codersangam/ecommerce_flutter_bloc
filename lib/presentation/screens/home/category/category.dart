dde035b55cfbcf803311735a26ccbf85aa072900
Sangam Singh <hritriik@gmail.com>
2022-07-07 22:53:03 +0400

    added category pages with related products

---
+++ lib/presentation/screens/home/category/category.dart
@@ -0,0 +1,36 @@
+part of 'category_imports.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({Key? key, required this.category}) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> categoryProducts = Product.products
+        .where((product) => product.category == category.name)
+        .toList();
+    return Scaffold(
+      appBar: VxAppBar(
+        elevation: 0.0,
+        searchBar: true,
+        centerTitle: true,
+        backgroundColor: Colors.orange.withOpacity(0.3),
+        title: '${category.name}'.text.make(),
+      ),
+      body: GridView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.7,
+          ),
+          itemCount: categoryProducts.length,
+          itemBuilder: (BuildContext context, index) {
+            return ProductsCard(
+              products: categoryProducts[index],
+              widthFactor: 1.5,
+            ).pSymmetric(h: 5);
+          }),
+    );
+  }
+}
