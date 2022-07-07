dfb6f254ecee7b7fc63061682048248da0b6f322
Sangam Singh <hritriik@gmail.com>
2022-07-07 22:52:12 +0400

    fixed route setup

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -3,7 +3,8 @@ part of 'router_imports.dart';
 @AdaptiveAutoRouter(
   routes: <AutoRoute>[
     //general routes
-    AdaptiveRoute(page: HomeScreen, initial: true),
+    AdaptiveRoute(page: MainScreen, initial: true),
+    AdaptiveRoute(page: CategoryScreen),
   ],
 )
 class $AppRouter {}
