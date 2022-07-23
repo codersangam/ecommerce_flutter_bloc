2ea20557b88abc20ec1b1d123be20670d1b8105b
Sangam Singh <hritriik@gmail.com>
2022-07-24 00:28:42 +0400

    Orders Bloc Added

---
+++ lib/blocs/order/bloc/order_event.dart
@@ -0,0 +1,43 @@
+part of 'order_bloc.dart';
+
+abstract class OrderEvent extends Equatable {
+  const OrderEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class UpdateOrdersEvent extends OrderEvent {
+  final String? fullName;
+  final String? email;
+  final String? phoneNumber;
+  final String? address;
+  final String? city;
+  final String? country;
+  final String? zipCode;
+  final Cart? cart;
+
+  const UpdateOrdersEvent({
+    this.fullName,
+    this.email,
+    this.phoneNumber,
+    this.address,
+    this.city,
+    this.country,
+    this.zipCode,
+    this.cart,
+  });
+
+  @override
+  List<Object?> get props =>
+      [fullName, email, phoneNumber, address, city, country, zipCode, cart];
+}
+
+class ConfirmOrdersEvent extends OrderEvent {
+  final Orders orders;
+
+  const ConfirmOrdersEvent({required this.orders});
+
+  @override
+  List<Object?> get props => [orders];
+}
