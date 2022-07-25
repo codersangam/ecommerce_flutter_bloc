ec02371be5e9b7cfddb72c304986cb48ca141f7b
Sangam Singh <hritriik@gmail.com>
2022-07-25 22:33:45 +0400

    orders UI added

---
+++ lib/presentation/screens/orders/orders_imports.dart
@@ -0,0 +1,7 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:ecommerce_flutter_bloc/presentation/router/router_imports.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+part 'orders.dart';
